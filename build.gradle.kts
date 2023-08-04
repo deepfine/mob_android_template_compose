2fac49c5cba7357476d44faece39f064aeef0d5b
yc.park <yc.park@deepfine.ai>
2023-08-04 14:41:48 +0900

    feat($project): Configure ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
