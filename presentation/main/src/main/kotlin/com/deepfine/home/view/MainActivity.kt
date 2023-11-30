2bee8bbe67180f7551817c4ca42bc907657cf6b9
Yechan Park <yc.park@deepfine.ai>
2023-11-30 15:51:08 +0900

    Set system bars configuration to ApplicationTheme

--- presentation/main/src/main/kotlin/com/deepfine/home/view/MainActivity.kt
+++ presentation/main/src/main/kotlin/com/deepfine/home/view/MainActivity.kt
@@ -3,12 +3,9 @@ package com.deepfine.home.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.deepfine.home.ui.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
-import tech.thdev.compose.exteions.system.ui.controller.rememberExSystemUiController
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -18,14 +15,6 @@ class MainActivity : ComponentActivity() {
     installSplashScreen()
 
     setContent {
-      val systemUIController = rememberExSystemUiController()
-
-      DisposableEffect(systemUIController) {
-        systemUIController.setSystemBarsColor(Color.White)
-
-        onDispose { }
-      }
-
       MainScreen()
     }
   }
--- presentation/src/main/kotlin/com/deepfine/presentation/ui/theme/ApplicationTheme.kt
+++ presentation/src/main/kotlin/com/deepfine/presentation/ui/theme/ApplicationTheme.kt
@@ -2,6 +2,9 @@ package com.deepfine.presentation.ui.theme
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.graphics.Color
+import tech.thdev.compose.exteions.system.ui.controller.rememberExSystemUiController
 
 /**
  * @Description
@@ -10,8 +13,26 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun ApplicationTheme(
+  systemBarsColor: Color = Color.White,
+  statusBarColor: Color = systemBarsColor,
+  navigationBarColor: Color = systemBarsColor,
+  isSystemBarVisible: Boolean = true,
+  isStatusBarVisible: Boolean = isSystemBarVisible,
+  isNavigationBarVisible: Boolean = isSystemBarVisible,
   content: @Composable () -> Unit
 ) {
+  val systemUIController = rememberExSystemUiController()
+
+  DisposableEffect(systemUIController) {
+    systemUIController.setStatusBarColor(statusBarColor)
+    systemUIController.setNavigationBarColor(navigationBarColor)
+    systemUIController.setSystemBarsColor(systemBarsColor)
+    systemUIController.isStatusBarVisible = isStatusBarVisible
+    systemUIController.isNavigationBarVisible = isNavigationBarVisible
+    systemUIController.isSystemBarsVisible = isSystemBarVisible
+    onDispose { }
+  }
+
   MaterialTheme(
     colorScheme = ColorScheme,
     typography = Typography,
