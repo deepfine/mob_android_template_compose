031aacf11491cca48cfb6e3c28e83d09ce374f8f
Yechan Park <yc.park@deepfine.ai>
2023-12-13 16:08:51 +0900

    Define buildconfig's gradle as plugin script

--- buildconfig-stub/build.gradle.kts
+++ buildconfig-stub/build.gradle.kts
@@ -1,21 +1,11 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-  id(libs.plugins.android.library.get().pluginId)
+  id(libs.plugins.deepfine.buildconfig.get().pluginId)
 }
 
 android {
-  namespace = "com.deepfine.buildconfig"
-
-  compileSdk = libs.versions.compileSdk.get().toInt()
-
   defaultConfig {
     buildConfigField("String", "VERSION_NAME", "String.valueOf(\"0\")")
     buildConfigField("String", "API_URL", "String.valueOf(\"\")")
   }
-
-  buildFeatures {
-    buildConfig = true
-  }
-
-  flavorDimensions.add("api")
 }
--- buildconfig/build.gradle.kts
+++ buildconfig/build.gradle.kts
@@ -1,22 +1,12 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-  id(libs.plugins.android.library.get().pluginId)
+  id(libs.plugins.deepfine.buildconfig.get().pluginId)
 }
 
 android {
-  namespace = "com.deepfine.buildconfig"
-
-  compileSdk = libs.versions.compileSdk.get().toInt()
-
   defaultConfig {
     buildConfigField("String", "VERSION_NAME", "String.valueOf(\"${libs.versions.versionName.get()}\")")
   }
-
-  buildFeatures {
-    buildConfig = true
-  }
-
-  flavorDimensions.add("api")
 
   productFlavors {
     // 개발계
@@ -28,5 +18,4 @@ android {
       buildConfigField("String", "API_URL", project.property("production.api.url").toString())
     }
   }
-
 }
